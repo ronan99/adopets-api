4a5bc40e40f6f723f47c91850add08ccf8f7e57a
ronan99 <ronan.lino99@gmail.com>
2020-07-08 20:40:11 -0400

    Update heroku options

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 const app = require("./app")
 
 
-app.listen( process.env.port || 3000 , console.log("Servidor rodando na porta : " + (process.env.port || 3000) ) );
+app.listen( process.env.PORT || 3000 , console.log("Servidor rodando na porta : " + (process.env.port || 3000) ) );
